4443cccd19bd38444f0cd9905b8bc046ce152a49
Stas <kostak363@gmail.com>
2022-12-10 10:15:47 +0100

    was

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// const h1 = <h1>Hello world</h1>
+const Title = () => {
+    return <h1>Hello React.js</h1>
+}
 
-const h1 = React.createElement(
-    'h1',
-    { id: 'title', className: 'test' },
-    'Hello React'
-)
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui,
+                repellat. Ut harum quidem tempore sint, est error vero impedit
+                beatae?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Provident tenetur aliquam deleniti fugiat.
+            </p>
+        </React.Fragment>
+    )
+}
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <Title />
+            <Content />
+        </React.Fragment>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{h1}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
