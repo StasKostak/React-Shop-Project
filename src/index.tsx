b7328aab913e91baa68fdd925fc0550cf790d71a
Stas <kostak363@gmail.com>
2022-12-11 10:18:33 +0100

    asd

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Content from './Content'
-
-type TitleProps = {
-    title: string
-    text?: string
-}
-
-const Title = ({ title, text = 'TEST' }: TitleProps) => {
-    return (
-        <h1>
-            {text} {title}
-        </h1>
-    )
-}
 
 const App = () => {
-    return (
-        <>
-            <Title text="Hello" title="About" />
-            <Content p1="asdasda" p2="khgasgkagg" />
-        </>
-    )
+    return <></>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
