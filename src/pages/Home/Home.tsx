96b1786463cfe4ac7e2c6dfb8a54c19599551aa3
Stas <kostak363@gmail.com>
2022-12-18 08:43:54 +0100

    Add Main

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material'
+
+type Props = {}
+
+const Home = (props: Props) => {
+    return (
+        <>
+            <Typography variant="h4" align="center">
+                {''}
+                List Product{''}
+            </Typography>
+        </>
+    )
+}
+export default Home
